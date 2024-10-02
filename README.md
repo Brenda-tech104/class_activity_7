925cc23e70c65381817805049b93a17b85c5b259
brenda s <brendasamano.5@gmail.com>
2024-10-02 18:27:55 -0400

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# class_activity_7
-group work 
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FadingTextAnimation(),
+    );
+  }
+}
+
+class FadingTextAnimation extends StatefulWidget {
+  const FadingTextAnimation({super.key});
+
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
+}
+
+class _FadingTextAnimationState extends State<FadingTextAnimation>
+    with SingleTickerProviderStateMixin {
+  bool _isVisible = true;
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _rotationAnimation =
+        Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+  }
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+
+    // Start rotation animation
+    if (_isVisible) {
+      _controller.forward(from: 0.0); // Rotate only when the text is visible
+    } else {
+      _controller.reverse(); // Optionally reverse the rotation when fading out
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fading Text Animation'),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: const Duration(seconds: 1),
+          child: AnimatedBuilder(
+            animation: _rotationAnimation,
+            child: const Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: _rotationAnimation.value,
+                child: child,
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
